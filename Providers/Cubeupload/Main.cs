1cfebaa7c264a6edcb684ce6dcb3fda322c769b0
Joe Biellik <joebiellik@gmail.com>
2016-12-07 20:49:50 +0000

    Add Cubeupload.com provider

---
+++ Providers/Cubeupload/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using ClipUp.Sdk;
+using ClipUp.Sdk.Providers;
+
+namespace ClipUp.Providers.Cubeupload
+{
+    public class Cubeupload : ImageUploadProvider
+    {
+        private const string UPLOAD_URL = "http://cubeupload.com/upload_json.php";
+
+        public override string Name => "Cubeupload";
+        public override Version Version => new Version(1, 0, 0);
+        public override string Link => "http://cubeupload.com/";
+        public override string Description => "cubeupload.com image upload";
+        public override long MaxSize => -1;
+
+        public override async Task<UploadResult> UploadImage(ImageUploadOptions options, Image image, IProgress<int> progress)
+        {
+            var multipartUpload = new MultipartUpload();
+
+            multipartUpload.Forms.Add(new KeyValuePair<string, object>("name", "upload.png"));
+            multipartUpload.Forms.Add(new KeyValuePair<string, object>("userHash", false));
+            multipartUpload.Forms.Add(new KeyValuePair<string, object>("userID", false));
+
+            multipartUpload.Files.Add(new MultipartFile
+            {
+                FieldName = "fileinput[0]",
+                ContentType = "image/png",
+                FileName = "upload.png",
+                Data = image.ToArray(options.Format)
+            });
+
+            var percent = 0;
+
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = multipartUpload.ContentType;
+                client.Headers.Add(HttpRequestHeader.UserAgent, options.UserAgent);
+
+                client.UploadProgressChanged += (s, e) =>
+                {
+                    if (progress == null) return;
+
+                    var current = (int)(e.BytesSent / (double)e.TotalBytesToSend * 100);
+
+                    if (current == percent) return;
+
+                    percent = current;
+                    progress.Report(current);
+                };
+
+                var result = await client.UploadDataTaskAsync(UPLOAD_URL, multipartUpload.RequestData);
+
+                var json = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(Encoding.Default.GetString(result));
+
+                return new UploadResult
+                {
+                    Success = true,
+                    Url = this.Link + "im/" + json["file_name"]
+                };
+            }
+        }
+    }
+}
---
+++ Providers/Cubeupload/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ClipUp Cubeupload Plugin")]
+[assembly: AssemblyDescription("ClipUp Cubeupload Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Joe Biellik")]
+[assembly: AssemblyProduct("ClipUp Cubeupload Plugin")]
+[assembly: AssemblyCopyright("Copyright © Joe Biellik 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("df08b192-2a5c-4608-9639-08abf675979b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
